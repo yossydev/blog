3936fbffacfbadb46e9004c2b3effdc81257b092
yossydev <codeyy021@gmail.com>
2024-02-25 15:45:30 +0900

    feat: dark color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./app/**/*.tsx"],
   theme: {
+    colors: {
+      ...colors,
+      black: {
+        DEFAULT: "#1c1c1c",
+      },
+    },
     extend: {},
   },
   plugins: [],
