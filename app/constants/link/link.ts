788477bceef96bf628dfbf5c98fe68045b717095
yossydev <codeyy021@gmail.com>
2024-02-25 15:46:42 +0900

    feat: constants

---
+++ app/constants/link/link.ts
@@ -0,0 +1,9 @@
+export const LINK = {
+  HOME: "/",
+  X: "https://twitter.com/yossydev",
+  GITHUB: "https://github.com/yossydev",
+  LINKEDIN: "https://www.linkedin.com/in/yutodev",
+  EMAIL: "codeyy021@gmail.com",
+  ZENN: "https://zenn.dev/yuto76",
+  AMAZON: "https://www.amazon.jp/hz/wishlist/ls/XSPLZ7OVMR3M?ref_=wl_share",
+};
