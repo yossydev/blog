c7b125bb4b8ddaf369a6ef26e71b5ccdcbc0a0a2
yossydev <codeyy021@gmail.com>
2024-03-06 04:13:48 +0900

    chore: descriptionの型を追加

--- app/global.d.ts
+++ app/global.d.ts
@@ -2,13 +2,14 @@ import {} from "hono";
 
 type Head = {
   title?: string;
+  description?: string;
 };
 
 declare module "hono" {
   interface ContextRenderer {
     (
       content: string | Promise<string>,
-      head?: Head & { frontmatter?: Head },
+      head?: Head & { frontmatter?: Head; description?: string },
     ): Response | Promise<Response>;
   }
 }
--- app/routes/posts/_renderer.tsx
+++ app/routes/posts/_renderer.tsx
@@ -2,7 +2,10 @@ import { jsxRenderer } from "hono/jsx-renderer";
 
 export default jsxRenderer(({ children, Layout, frontmatter }) => {
   return (
-    <Layout title={`${frontmatter?.title} | yossy.dev`}>
+    <Layout
+      title={`${frontmatter?.title} | yossy.dev`}
+      description={frontmatter?.description}
+    >
       <div class="markdown">{children}</div>
     </Layout>
   );
