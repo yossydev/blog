cba3bbb1536657cb67d773743b862fcaa4a3a495
yossydev <codeyy021@gmail.com>
2024-03-05 23:23:54 +0900

    feat: 画像の設定

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,8 +1,7 @@
 import { Style } from "hono/css";
-import { type FC } from "hono/jsx";
 import { jsxRenderer } from "hono/jsx-renderer";
 import { Script } from "honox/server";
-import { LINK } from "../constants";
+import { Header } from "../islands/Header";
 
 export default jsxRenderer(({ children, title }) => {
   return (
@@ -32,36 +31,3 @@ export default jsxRenderer(({ children, title }) => {
     </html>
   );
 });
-
-const Header: FC = () => {
-  return (
-    <div className="mt-3 bg-black">
-      <div className="max-w-screen-2xl mx-auto flex h-16 items-center justify-between px-6">
-        <a href={"/"} className="text-white text-base font-bold">
-          Yuto Blog
-        </a>
-        <div className="flex items-center gap-2">
-          <a href={LINK.X} target={"_blank"} rel={"noreferrer"} className="p-2">
-            <img
-              src="/app/images/twitter-alt.svg"
-              alt="x-icon"
-              className="w-7 h-7"
-            />
-          </a>
-          <a
-            href={LINK.GITHUB}
-            target={"_blank"}
-            rel="noreferrer"
-            className="p-2"
-          >
-            <img
-              src="/app/images/github.svg"
-              alt="github-icon"
-              className="w-7 h-7 text-white"
-            />
-          </a>
-        </div>
-      </div>
-    </div>
-  );
-};
--- vite.config.ts
+++ vite.config.ts
@@ -33,5 +33,8 @@ export default defineConfig(({ mode }) => {
       }),
       ssg({ entry }),
     ],
+    build: {
+      outDir: "public",
+    },
   };
 });
