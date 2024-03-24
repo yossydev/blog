e07d34b47126a6d103416d1046d10efc57a8722e
yossydev <codeyy021@gmail.com>
2024-03-24 17:31:47 +0900

    feat: toc

--- app/routes/posts/_renderer.tsx
+++ app/routes/posts/_renderer.tsx
@@ -6,7 +6,10 @@ export default jsxRenderer(({ children, Layout, frontmatter }) => {
   const _title = `${frontmatter?.title} | yossy.dev`;
   return (
     <Layout title={_title} description={frontmatter?.description}>
-      <div class="markdown">{children}</div>
+      <div class="markdown">
+        <h1>{frontmatter?.title}</h1>
+        {children}
+      </div>
       <LikeButton />
       <SnsButton title={_title} />
     </Layout>
