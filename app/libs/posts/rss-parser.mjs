6b08cb2be849367082a172be6fc2686d18041f66
Yuto Yoshino <yossydev@yossy.dev>
2024-11-04 10:04:02 +0900

    update rss (#54)
    Co-authored-by: yossydev <codeyy021@gmail.com>

--- app/libs/posts/rss-parser.mjs
+++ app/libs/posts/rss-parser.mjs
@@ -4,12 +4,13 @@ const parser = new Parser();
 
 (async () => {
   let jsonFeed = {};
-  const [feed1, feed2, feed3] = await Promise.all([
+  const [feed1, feed2, feed3, caRss] = await Promise.all([
     parser.parseURL("https://zenn.dev/yuto76/feed"),
     parser.parseURL(
       "https://www.youtube.com/feeds/videos.xml?channel_id=UCu6ckjaLjqCKY-c01fEqUqw",
     ),
     parser.parseURL("https://speakerdeck.com/yossydev.rss"),
+    parser.parseURL("https://speakerdeck.com/cyberagentdevelopers.rss"),
   ]);
   const items = feed1.items.map((data) => {
     return data;
@@ -26,6 +27,21 @@ const parser = new Parser();
   const items3 = feed3.items.map((data) => {
     return data;
   });
-  jsonFeed = items3;
+  const items4 = caRss.items
+    .map((data) => {
+      return data;
+    })
+    .filter((data) => {
+      return (
+        data.link ===
+        "https://speakerdeck.com/cyberagentdevelopers/fantech-web-biome"
+      );
+    });
+
+  jsonFeed = [...items3, ...items4].sort((a, b) => {
+    const dateA = new Date(a.isoDate).getTime();
+    const dateB = new Date(b.isoDate).getTime();
+    return dateB - dateA;
+  });
   writeFileSync("app/libs/rss/speakerdeck.json", JSON.stringify(jsonFeed));
 })();
