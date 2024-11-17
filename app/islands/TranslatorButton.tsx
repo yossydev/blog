d088ed1142fdd64435455735bac6d56e8a70273a
Yuto Yoshino <yossydev@yossy.dev>
2024-11-17 15:40:38 +0900

    Support Translator-api (#57)
    * feat: Translator-api
    
    * chore: language option
    
    * chore: remove select
    
    * fix: lint

---
+++ app/islands/TranslatorButton.tsx
@@ -0,0 +1,74 @@
+import { type FC, useState } from "hono/jsx";
+
+type SupportedTranslatorAPI =
+  | "en"
+  | "zn"
+  | "ja"
+  | "pt"
+  | "ru"
+  | "es"
+  | "tr"
+  | "hi"
+  | "vi"
+  | "bn";
+
+const SELECT_OPTIONS: SupportedTranslatorAPI[] = [
+  "ja",
+  "en",
+  "zn",
+  "pt",
+  "ru",
+  "es",
+  "tr",
+  "hi",
+  "vi",
+  "bn",
+];
+
+type Props = {
+  content: string;
+};
+
+const TranslatorButton: FC<Props> = ({ content }) => {
+  const [value, setValue] = useState<SupportedTranslatorAPI>("ja");
+
+  // biome-ignore lint: lint/suspicious/noExplicitAny
+  const handleSelectChange = async (e: any) => {
+    setValue(e.target.value);
+    if (
+      "translation" in self &&
+      // biome-ignore lint: lint/suspicious/noExplicitAny
+      "createTranslator" in (self.translation as any)
+    ) {
+      // biome-ignore lint: lint/suspicious/noExplicitAny
+      const translator = await (self.translation as any).createTranslator({
+        sourceLanguage: value,
+        targetLanguage: e.target.value,
+      });
+      await translator.translate(content);
+    } else {
+      window.alert("The Translator API is not supported");
+    }
+  };
+
+  return (
+    <>
+      <label for="pet-select">translate to: </label>
+      <select
+        onChange={handleSelectChange}
+        value={value}
+        class="cursor-pointer"
+      >
+        {SELECT_OPTIONS.map((value) => {
+          return (
+            <option key={value} value={value}>
+              {value}
+            </option>
+          );
+        })}
+      </select>
+    </>
+  );
+};
+
+export default TranslatorButton;
