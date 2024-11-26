6e3d2fce8293e2339c61dd5c52ea22c2d7a7817f
yossydev <yossydev@yossy.dev>
2024-11-26 21:04:19 +0900

    update profile

--- app/routes/profile/index.tsx
+++ app/routes/profile/index.tsx
@@ -3,53 +3,6 @@ import { createRoute } from "honox/factory";
 import { Heading } from "../../components/Heading";
 import { LINK } from "../../constants";
 
-const SnsList: {
-  id: number;
-  title: string;
-  href: string;
-}[] = [
-  {
-    id: 1,
-    title: "X",
-    href: LINK.X,
-  },
-  {
-    id: 2,
-    title: "BlueSky",
-    href: LINK.BLUESKY,
-  },
-  {
-    id: 3,
-    title: "GitHub",
-    href: LINK.GITHUB,
-  },
-  {
-    id: 4,
-    title: "LinkedIn",
-    href: LINK.LINKEDIN,
-  },
-  {
-    id: 6,
-    title: "Email",
-    href: LINK.EMAIL,
-  },
-  {
-    id: 7,
-    title: "Zenn",
-    href: LINK.ZENN,
-  },
-  {
-    id: 8,
-    title: "Youtube",
-    href: LINK.YOUTUBE,
-  },
-  {
-    id: 9,
-    title: "Speakerdeck",
-    href: LINK.SPEAKERDECK,
-  },
-];
-
 const Text: FC<{ text: string }> = ({ text }) => {
   return <p class="text-lg font-medium mt-6">{text}</p>;
 };
@@ -58,7 +11,7 @@ export default createRoute((c) => {
   return c.render(
     <>
       <Heading title="About Me" />
-      <Text text="都内在住の技術好きなエンジニアです。出身は日本です。TypeScriptを主に扱っており、最近ではRust / Developer Experience / OSS開発に興味があります。" />
+      <Text text="都内在住の技術好きなエンジニアです。出身は日本です。TypeScriptを主に扱っており、最近ではWeb周辺のエコシステムに興味があります。" />
       <br />
       自分のアウトプットに関しては、
       <a href={"/"} class="underline-offset-2 underline mr-1 text-blue-600">
@@ -95,21 +48,6 @@ export default createRoute((c) => {
       <Text
         text={`お仕事や登壇のご依頼などはXのDMもしくは${LINK.EMAIL}までご連絡ください。`}
       />
-      <br />
-      <Heading title="Link" />
-      <ul class="text-lg mt-6">
-        {SnsList.map(({ id, title, href }) => (
-          <li key={id} class="flex items-center py-1">
-            <div class="w-2 h-2 rounded bg-black mr-2" />
-            <a
-              href={href}
-              class="underline-offset-2 underline mr-1 text-blue-600"
-            >
-              {title}
-            </a>
-          </li>
-        ))}
-      </ul>
     </>,
     { title: "Profile | yossy.dev" },
   );
